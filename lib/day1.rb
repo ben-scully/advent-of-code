710cc165a953610ab1ef584d6dafa279b728fe90
ben-scully <ben@moregolf.co.nz>
2017-10-19 16:53:22 +1300

    day1 complete

---
+++ lib/day1.rb
@@ -0,0 +1,57 @@
+# lib/day1.rb
+
+class Day1
+  NORTH = 'north'
+  EAST = 'east'
+  SOUTH = 'south'
+  WEST = 'west'
+
+  def self.blocks_away(sequence)
+    seqs = sequences(sequence)
+
+    dirs_and_blks = directions_and_blocks(NORTH, seqs, [])
+
+    norths  = blocks_per_direction(NORTH, dirs_and_blks)
+    easts   = blocks_per_direction(EAST,  dirs_and_blks)
+    souths  = blocks_per_direction(SOUTH, dirs_and_blks)
+    wests   = blocks_per_direction(WEST,  dirs_and_blks)
+
+    (norths - souths).abs + (wests - easts).abs
+  end
+
+  def sequences(sequence)
+    sequence.split(', ').map { |item| {turn: item[0], blocks: item[1..-1].to_i } }
+  end
+
+  def self.directions_and_blocks(previous_facing, sequence, new_sequence)
+    return new_sequence if sequence.empty?
+
+    next_dir = sequence.shift
+
+    now_facing = direction_after_turn(previous_facing, next_dir[:turn] == 'R')
+    new_sequence.push({direction: now_facing, blocks: next_dir[:blocks]})
+
+    directions_and_blocks(now_facing, sequence, new_sequence)
+  end
+
+  def self.direction_after_turn(direction, turn_clockwise)
+    case direction
+    when NORTH
+        turn_clockwise ? EAST : WEST
+    when EAST
+        turn_clockwise ? SOUTH : NORTH
+    when SOUTH
+        turn_clockwise ? WEST : EAST
+    when WEST
+        turn_clockwise ? NORTH : SOUTH
+    else
+      raise 'Invalid direction given'
+    end
+  end
+
+  def self.blocks_per_direction(direction, directions_and_blocks)
+    directions_and_blocks
+      .select { |item| item[:direction] == direction }
+      .sum    { |item| item[:blocks] }
+  end
+end
---
+++ spec/day1_spec.rb
@@ -0,0 +1,43 @@
+# spec/day1_spec.rb
+
+require 'day1'
+
+describe Day1 do
+  describe '#blocks_away' do
+    context 'R2, L3' do
+      let(:sequence) { 'R2, L3' }
+      let(:blocks_away) { 5 }
+
+      it 'returns 5 blocks away' do
+        expect(Day1.blocks_away(sequence)).to eq(blocks_away)
+      end
+    end
+
+    context 'R2, R2, R2' do
+      let(:sequence) { 'R2, R2, R2' }
+      let(:blocks_away) { 2 }
+
+      it 'returns 2 blocks away' do
+        expect(Day1.blocks_away(sequence)).to eq(blocks_away)
+      end
+    end
+
+    context 'R5, L5, R5, R3' do
+      let(:sequence) { 'R5, L5, R5, R3' }
+      let(:blocks_away) { 12 }
+
+      it 'returns 12 blocks away' do
+        expect(Day1.blocks_away(sequence)).to eq(blocks_away)
+      end
+    end
+
+    context 'R*, L*, L* ...' do
+      let(:sequence) { 'L5, R1, R4, L5, L4, R3, R1, L1, R4, R5, L1, L3, R4, L2, L4, R2, L4, L1, R3, R1, R1, L1, R1, L5, R5, R2, L5, R2, R1, L2, L4, L4, R191, R2, R5, R1, L1, L2, R5, L2, L3, R4, L1, L1, R1, R50, L1, R1, R76, R5, R4, R2, L5, L3, L5, R2, R1, L1, R2, L3, R4, R2, L1, L1, R4, L1, L1, R185, R1, L5, L4, L5, L3, R2, R3, R1, L5, R1, L3, L2, L2, R5, L1, L1, L3, R1, R4, L2, L1, L1, L3, L4, R5, L2, R3, R5, R1, L4, R5, L3, R3, R3, R1, R1, R5, R2, L2, R5, L5, L4, R4, R3, R5, R1, L3, R1, L2, L2, R3, R4, L1, R4, L1, R4, R3, L1, L4, L1, L5, L2, R2, L1, R1, L5, L3, R4, L1, R5, L5, L5, L1, L3, R1, R5, L2, L4, L5, L1, L1, L2, R5, R5, L4, R3, L2, L1, L3, L4, L5, L5, L2, R4, R3, L5, R4, R2, R1, L5' }
+      let(:blocks_away) { 242 }
+
+      it 'returns 12 blocks away' do
+        expect(Day1.blocks_away(sequence)).to eq(blocks_away)
+      end
+    end
+  end
+end
