59d84018d99d5f71314c275438a7c0c09ba341bd
ben-scully <ben@moregolf.co.nz>
2017-10-20 16:11:53 +1300

    update day4

--- lib/day4.rb
+++ lib/day4.rb
@@ -1,5 +1,19 @@
 # lib/day4.rb
 
 class Day4
-  
+  def self.sector_id_sum(sector_ids)
+
+  end
+
+  def self.sector_id(sector_id)
+
+  end
+
+  def self.valid_sector_id?(sector_id)
+
+  end
+
+  def self.character_count(sector_id)
+    sector_id.split('-')
+  end
 end
--- spec/day4_spec.rb
+++ spec/day4_spec.rb
@@ -5,15 +5,112 @@ require 'day4'
 describe Day4 do
   subject(:day) { described_class }
 
-  # describe '#possible_triangles' do
-  #   context 'when given text list: short' do
-  #     let(:count) { 0 }
-  #     let(:list) { '  541  588  421
-  #     827  272  126' }
-  #     let(:result) { 1 }
+  # describe '#valid_sector_id?' do
+  #   context 'when valid' do
+  #     let(:sector_id) { 'aaaaa-bbb-z-y-x-123[abxyz]' }
   #
-  #     it 'returns possible triangle count' do
-  #       expect(day.possible_triangles(list)).to eql(result)
+  #     it 'returns valid?' do
+  #       expect(day.valid_sector_id?(sector_id)).to be true
+  #     end
+  #   end
+  #
+  #   context 'when valid' do
+  #     let(:sector_id) { 'a-b-c-d-e-f-g-h-987[abcde]' }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.valid_sector_id?(sector_id)).to be true
+  #     end
+  #   end
+  #
+  #   context 'when valid' do
+  #     let(:sector_id) { 'not-a-real-room-404[oarel]' }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.valid_sector_id?(sector_id)).to be true
+  #     end
+  #   end
+  #
+  #   context 'when invalid' do
+  #     let(:sector_id) { 'totally-real-room-200[decoy]' }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.valid_sector_id?(sector_id)).to be false
+  #     end
+  #   end
+  # end
+
+  describe '#character_count' do
+    context 'when valid' do
+      let(:sector_id) { 'aaaaa-bbb-z-y-x-123[abxyz]' }
+      let(:result) { [] }
+
+      it 'returns character_count' do
+        expect(day.character_count(sector_id)).to eql(result)
+      end
+    end
+
+    context 'when valid' do
+      let(:sector_id) { 'a-b-c-d-e-f-g-h-987[abcde]' }
+      let(:result) { [] }
+
+      it 'returns character_count' do
+        expect(day.character_count(sector_id)).to eql(result)
+      end
+    end
+
+    context 'when valid' do
+      let(:sector_id) { 'not-a-real-room-404[oarel]' }
+      let(:result) { [] }
+
+      it 'returns character_count' do
+        expect(day.character_count(sector_id)).to eql(result)
+      end
+    end
+
+    context 'when invalid' do
+      let(:sector_id) { 'totally-real-room-200[decoy]' }
+      let(:result) { [] }
+
+      it 'returns character_count' do
+        expect(day.character_count(sector_id)).to eql(result)
+      end
+    end
+  end
+
+  # describe '#sector_id' do
+  #   context 'when valid' do
+  #     let(:sector_id) { 'aaaaa-bbb-z-y-x-123[abxyz]' }
+  #     let(:result) { 123 }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.sector_id(sector_id)).to eql(result)
+  #     end
+  #   end
+  #
+  #   context 'when valid' do
+  #     let(:sector_id) { 'a-b-c-d-e-f-g-h-987[abcde]' }
+  #     let(:result) { 987 }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.sector_id(sector_id)).to eql(result)
+  #     end
+  #   end
+  #
+  #   context 'when valid' do
+  #     let(:sector_id) { 'not-a-real-room-404[oarel]' }
+  #     let(:result) { 404 }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.sector_id(sector_id)).to eql(result)
+  #     end
+  #   end
+  #
+  #   context 'when invalid' do
+  #     let(:sector_id) { 'totally-real-room-200[decoy]' }
+  #     let(:result) { 200 }
+  #
+  #     it 'returns valid?' do
+  #       expect(day.sector_id(sector_id)).to eql(result)
   #     end
   #   end
   # end
